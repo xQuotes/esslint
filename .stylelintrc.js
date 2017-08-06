4b261acc27932c41d92689bed0d0d2d0bc85bec6
fuyin [付引] <fuyin@xiaomi.com>
2017-08-07 00:05:26 +0800

    style config

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //https://stylelint.io/user-guide/rules/
+  extends: 'stylelint-config-standard',
+  rules: {
+    indentation: 2, // tab/1 2(space)
+    'number-leading-zero': null
+  }
+}
